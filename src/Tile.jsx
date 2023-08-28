5e456686af6e6aad183fd2d9e5169261a3d6e8b1
lMuzal <muza.jakub@gmail.com>
2023-08-29 00:39:39 +0200

    some more tile stylig

--- src/Tile.jsx
+++ src/Tile.jsx
@@ -6,7 +6,7 @@ export default function Tile(props){
       <div className="pt-3">
         <label>
           <input type="radio" className="peer sr-only" name={props.tileName} />
-          <div className=" text-amber-400 border-2 font-bold tracking-wider border-amber-400 py-1 px-5 mx-1 rounded ease-in-out duration-300 hover:text-lime-900 hover:bg-amber-400">{props.tileName}</div>
+          <div className=" text-amber-400 border sm:border-2 font-bold tracking-wider border-amber-400 py-1 px-5 mx-1 rounded ease-in-out duration-300 hover:text-lime-900 hover:bg-amber-400">{props.tileName}</div>
         </label>
       </div>
   )
