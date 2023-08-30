237c354f1f19c08e1d72e12dd7951d2789990f80
lMuzal <muza.jakub@gmail.com>
2023-08-30 04:00:10 +0200

    add: some degree of element's visibility

--- src/Tile.jsx
+++ src/Tile.jsx
@@ -1,12 +1,18 @@
 /* eslint-disable react/prop-types */
 export default function Tile(props){
+
+  let classVisibility = '';
+  
+
+  // if (props.classNameDivision == "thirdDivision" || props.classNameDivision =="secondDivision"){
+  //   classVisibility = "hidden";
+  // }
   
   return(
-
-      <div className="pt-3">
+      <div className={`pt-3  ${props.classNameDivision} ${classVisibility}` }>
         <label>
-          <input type="radio" className="peer sr-only" name={props.category}/>
-          <div className="text-amber-400 border sm:border-2 font-bold tracking-wider border-amber-400 py-1 px-5 mx-1 rounded ease-in-out duration-300 hover:text-lime-900 hover:bg-amber-400 peer-checked:text-lime-900 peer-checked:bg-amber-400">{props.tileName}</div>
+          <input type="radio" className='radio-button peer sr-only' name={props.category}/>
+          <div className="text-amber-400 border sm:border-2 font-bold tracking-wider border-amber-400 py-1/2 px-5 mx-1 rounded ease-in-out duration-300 hover:text-lime-900 hover:bg-amber-400 peer-checked:text-lime-900 peer-checked:bg-amber-500">{props.tileName}</div>
         </label>
       </div>
   )
